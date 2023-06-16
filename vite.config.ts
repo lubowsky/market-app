b3fd627cd6084a6309b6b63cf481fa477c2707cb
Осипова Виктория <lubowsky81@yandex.ru>
2023-06-16 11:47:19 +0700

    fix font

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,46 @@ import eslint from 'vite-plugin-eslint';
 import path from 'path';
 
 
+// export default defineConfig({
+//   plugins: [react(), eslint()],
+//   base: '/market-app',
+//   build: {
+//     outDir: 'build',
+//     assetsDir: 'static',
+//     rollupOptions: {
+//       output: {
+//         entryFileNames: `[name].[hash].js`,
+//         chunkFileNames: `[name].[hash].js`,
+//         assetFileNames: (assetInfo) => {
+//           const ext = path.extname(assetInfo.name)
+//           return `static/${ext.slice(1)}/${assetInfo.name}`
+//         },
+//       },
+//     },
+//   },
+// })
+
+// import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react(), eslint()],
-  base: '/market-app',
   build: {
     outDir: 'build',
     assetsDir: 'static',
     rollupOptions: {
       output: {
-        entryFileNames: `[name].[hash].js`,
-        chunkFileNames: `[name].[hash].js`,
-        assetFileNames: (assetInfo) => {
-          const ext = path.extname(assetInfo.name)
-          return `static/${ext.slice(1)}/${assetInfo.name}`
+        assetFileNames: ({ name }) => {
+          if (name.endsWith('.js')) {
+            return 'static/js/[name].[hash][extname]';
+          } else if (name.endsWith('.css')) {
+            return 'static/css/[name].[hash][extname]';
+          } else if (/\.(png|jpe?g|gif|svg|webp)$/i.test(name)) {
+            return 'static/img/[name].[hash][extname]';
+          } else {
+            return 'static/[name].[hash][extname]';
+          }
         },
       },
     },
   },
-})
+});
