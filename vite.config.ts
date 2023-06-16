99bbc5aea2d8b9572a4cbb36b3d3037dc08ec300
Осипова Виктория <lubowsky81@yandex.ru>
2023-06-16 12:06:30 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
-import path from 'path';
-
-
-// export default defineConfig({
-//   plugins: [react(), eslint()],
-//   base: '/market-app',
-//   build: {
-//     outDir: 'build',
-//     assetsDir: 'static',
-//     rollupOptions: {
-//       output: {
-//         entryFileNames: `[name].[hash].js`,
-//         chunkFileNames: `[name].[hash].js`,
-//         assetFileNames: (assetInfo) => {
-//           const ext = path.extname(assetInfo.name)
-//           return `static/${ext.slice(1)}/${assetInfo.name}`
-//         },
-//       },
-//     },
-//   },
-// })
-
-// import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), eslint()],
